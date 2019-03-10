65be9eee07b21ac26359e44dcf219f87d798c7c8
Zachary Lehmann <zlehmann@gmail.com>
2019-03-10 19:46:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const driver = {
 };
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  driver[key] = value;
+  const newDriver = { ...driver};
+  newDriver[key] = value;
+  return newDriver;
 }
